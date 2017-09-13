40c965239f79888a5ba399f3efd94b13527f1271
RiverTaig <river.taig@gmail.com>
2017-09-13 12:17:33 -0800

    Modified method by which image rotation works and modified some css

--- assets/js/river.js
+++ assets/js/river.js
@@ -1,6 +1,29 @@
-﻿$(document).ready(function () {
-    $("#cf_onclick").click(function () {
-        //alert("e");
-        $("#cf2 img.top").toggleClass("transparent");
-    });
+﻿$(window).ready(function () {
+    var InfiniteRotator = {
+        init: function () {
+            //initial fade-in time (in milliseconds)
+            var initialFadeIn = 1000;
+            //interval between items (in milliseconds)
+            var itemInterval = 5000;
+            //cross-fade time (in milliseconds)
+            var fadeTime = 2500;
+            //count number of items
+            var numberOfItems = $('.rotating-item').length;
+            //set current item
+            var currentItem = 0;
+            //show first item
+            $('.rotating-item').eq(currentItem).fadeIn(initialFadeIn);
+            //loop through the items
+            var infiniteLoop = setInterval(function () {
+                $('.rotating-item').eq(currentItem).fadeOut(fadeTime);
+                if (currentItem == numberOfItems - 1) {
+                    currentItem = 0;
+                } else {
+                    currentItem++;
+                }
+                $('.rotating-item').eq(currentItem).fadeIn(fadeTime);
+            }, itemInterval);
+        }
+    };
+    InfiniteRotator.init();
 });
